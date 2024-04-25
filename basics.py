c600fe93f8757a228e43a81906b06b047737e95a
Sagnik Nandi <backup1.sagnik@gmail.com>
2024-04-26 00:28:54 +0530

    Running camera on basics

--- basics.py
+++ basics.py
@@ -5,18 +5,48 @@ pygame.init()
 
 screen = pygame.display.set_mode((800, 800))
 pygame.display.set_caption("Let's make a game!")
+clock=pygame.time.Clock()
+
+player_rect=pygame.Rect(0,0,400,400)
+
+image=pygame.image.load("./Images/StartScreen.png")
+image=pygame.transform.scale(image, (800,800))
+sub_image=image.subsurface(player_rect)
+screen.blit(sub_image, player_rect.topleft)
+pygame.display.update()
 
 x=40
 y=40
 w=30
 h=30
+player1=pygame.draw.rect(screen, (0,255,0), (x,y,w,h))
 vel=5
-clock=pygame.time.Clock()
 
 run=True
 while run:
     clock.tick(300)
     pygame.time.delay(50) #this is required for moving with keys unless those keys are seen as an event
+    screen.fill((0,0,0), player1) #after each frame screen is updated to blank.. so as to create moving effect.. otherwise it will create overlapping rectangles without any moving effect
+    screen.fill((0,0,0), player_rect)
+
+    # Setting up the camera
+    if player_rect.centerx>player1.x>200:
+        player_rect.centerx=player1.x
+    if player_rect.centerx<player1.x<600:
+        player_rect.centerx=player1.x
+    if player_rect.centery>player1.y and player_rect.top>0:
+        player_rect.centery=player1.y    
+    if player_rect.centery<player1.y and player_rect.bottom<800:
+        player_rect.centery=player1.y
+
+
+    sub_image=image.subsurface(player_rect)
+    screen.blit(sub_image, player_rect.topleft)
+
+    # camera=image.subsurface(player_rect)
+    # camera.unlock()
+    # screen.blit(camera, player_rect.topleft)
+
 
     # Watch for keyboard and mouse events.
     for event in pygame.event.get():
@@ -34,6 +64,5 @@ while run:
     if keys[pygame.K_DOWN] and y<=800-h-vel:
         y+=vel
 
-    screen.fill((0,0,0)) #after each frame screen is updated to blank.. so as to create moving effect.. otherwise it will create overlapping rectangles without any moving effect
-    pygame.draw.rect(screen, (0,255,0), (x,y,w,h))
+    player1=pygame.draw.rect(screen, (0,255,0), (x,y,w,h))
     pygame.display.update()
